86bd701374a0c837805b7185b36431fdc22a1d55
fname <email@gmail.com>
2022-04-03 10:41:31 +0530

    updateds

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 var baseURL;
-    baseURL = 'http://127.0.0.1:8000';
+    baseURL = 'https://backend-sdp-m.herokuapp.com/';
 
 const api = axios.create({
     baseURL: baseURL,

a067e114b80621c2d9e395b07ec3ac2a96c14dba
fname <email@gmail.com>
2022-04-04 15:31:33 +0530

    updated chenges

--- frontend/src/components/Common/Cover.jsx
+++ frontend/src/components/Common/Cover.jsx
@@ -4,11 +4,11 @@ import button from '../../assets/img/button.png'
 
 function Cover() {
   return (
-    <div>
+    <div class="main-cover">
         <Header/>
         <section class="cover">
             <div class="cover-info">
-                <a href="https://www.youtube.com/watch?v=vw2FOYjCz38"><img src={button} alt="" /></a>
+                <a href="https://www.youtube.com/watch?v=vw2FOYjCz38" target='_blank' ><img src={button} alt="" /></a>
                 <pre>
                     October 1st
                     In Cinemas
@@ -16,8 +16,8 @@ function Cover() {
                 <br />
                 <br />
                 <div class="description">
-                    James Bond has left active service. His peace is short-lived when Felix Leiter,<br />
-                    an oldfriend from the CIA, turns up asking for help, leading Bond ontobr <br />
+                    James Bond has left active service. His peace is short-lived when Felix Leiter,
+                    an oldfriend from the CIA, turns up asking for help, leading Bond ontobr
                     the trail of a mysterious villain armed with dangerous new technology.
                 </div>
             </div>      
